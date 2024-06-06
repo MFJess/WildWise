93ee00eb660ac0909f4a811fb1419ff474be7aed
VitorLelis <vitorll909@gmail.com>
2024-06-06 19:06:01 +0100

    feat(app): flask setup

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Data do sistema no formato ISO
+data_hora_atual = datetime.now()
+data_formatada = data_hora_atual.strftime('%Y-%m-%d T %H: %M: %S')
+
+# Index
+@app.route('/')
+def index():
+    return render_template('index.html', data = {"data": data_formatada})
+
+# Consulta
+@app.route('/consulta')
+def consulta():
+    return render_template('consulta.html', data = {"data": data_formatada})
+
+if __name__ == '__main__':
+    app.run(debug=True)
