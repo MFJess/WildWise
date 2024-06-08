be79c7260fa6a7fd8f2ca9368128d69477ed5f8b
VitorLelis <vitorll909@gmail.com>
2024-06-08 14:18:31 +0100

    feat(forms): forms page

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -16,6 +16,16 @@ def index():
 @app.route('/consulta')
 def consulta():
     return render_template('consulta.html', data = {"data": data_formatada})
+
+# Aumentar
+@app.route('/aumentar', methods=['POST','GET'])
+def aumentar():
+    if request.method == 'POST':
+        forms = dict(request.form)
+
+        return render_template('success.html', data = {"data": data_formatada,"name":forms['name']})
+    else:
+        return render_template('aumentar.html', data = {"data": data_formatada})
 
 if __name__ == '__main__':
     app.run(debug=True)
